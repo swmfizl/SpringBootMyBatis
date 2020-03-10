7d3ea1b6ba8f31e15e7d6d98343d03509ca7c9d8
zhongli <542881096@qq.com>
2020-03-10 12:25:13 +0800

    v1.0.0

--- src/main/java/com/mybatis/controller/UserController.java
+++ src/main/java/com/mybatis/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mybatis.entity.User;
@@ -14,32 +15,32 @@ import com.mybatis.service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService;
-	
+
 	@ResponseBody
-	@RequestMapping("insert")
+	@RequestMapping(value = "insert", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
 	public int insert() {
 		// 返回新增成功的行数
 		return userService.insert(new User("钟力", "Java高级工程师"));
 	}
-	
+
 	@ResponseBody
-	@RequestMapping("delete")
+	@RequestMapping(value = "delete", method = RequestMethod.DELETE, produces = "application/json;charset=utf-8")
 	public int delete() {
 		// 返回删除成功的行数
 		return userService.delete(1);
 	}
-	
+
 	@ResponseBody
-	@RequestMapping("update")
+	@RequestMapping(value = "update", method = RequestMethod.PUT, produces = "application/json;charset=utf-8")
 	public int update() {
 		User user = new User("钟力", "Java架构师");
 		user.setId(1);
 		// 返回修改成功的行数
 		return userService.update(user);
 	}
-	
+
 	@ResponseBody
-	@RequestMapping("select")
+	@RequestMapping(value = "select", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
 	public List<User> select() {
 		return userService.select();
 	}
